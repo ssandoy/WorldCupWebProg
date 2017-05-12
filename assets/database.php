de1c37e2d48257f2aab217426b982f623718947e
Sindre Beba <sindre.beba@gmail.com>
2017-05-12 17:12:47 +0200

    Ryddet opp i database.php

--- assets/database.php
+++ assets/database.php
@@ -2,13 +2,106 @@
 
 include("classes.php");
 
-// REGISTER
+//======================================================================
+// DELETE FUNCTIONS
+//======================================================================
 
-// EDIT
+//======================================================================
+// EDIT FUNCTIONS
+//======================================================================
 
-// DELETE
+//======================================================================
+// GET FUNCTIONS
+//======================================================================
 
-// POPULATE
+function getAdminInfo() {
+    // Connect to database.
+    $db = new mysqli("student.cs.hioa.no", "s236305", "", "s236305");
+    if ($db->connect_error) {
+        trigger_error($db->connect_error);
+    }
+    // Get the ID of the logged in admin. TODO
+    $id = 1;
+    // Execute SQL query.
+    $sql = "SELECT username, firstname, lastname, phonenumber "
+        . "FROM Admin "
+        . "WHERE AdminID = $id;";
+    $result = $db->query($sql);
+    // Echo out admin info.
+    if($db->affected_rows>0) {
+        $row = $result->fetch_object();
+        echo "<h4>Username:</h4><p>$row->username</p>"
+            . "<h4>Name:</h4><p>$row->firstname $row->lastname</p>"
+            . "<h4>Phone number:</h4><p>$row->phonenumber</p>";
+    } else {
+        echo "<p>Could not find admin info.</p>";
+    }
+    // Close database connection.
+    $db->close();
+}
+
+function getSpectatorInfo() {
+    // Connect to database.
+    $db = new mysqli("student.cs.hioa.no", "s236305", "", "s236305");
+    if ($db->connect_error) {
+        trigger_error($db->connect_error);
+    }
+    // Get the ID of the logged in spectator. TODO
+    $id = 1;
+    // Execute SQL query.
+    $sql = "SELECT firstname, lastname, phonenumber, email, username "
+        . "FROM Spectator "
+        . "WHERE SpectatorID = $id;";
+    $result = $db->query($sql);
+    // Echo out spectator info.
+    if($db->affected_rows>0) {
+        $row = $result->fetch_object();
+        echo "<h4>Username:</h4><p>$row->username</p>"
+            . "<h4>Name:</h4><p>$row->firstname $row->lastname</p>"
+            . "<h4>Phone number:</h4><p>$row->phonenumber</p>"
+            . "<h4>E-mail:</h4><p>$row->email</p>";
+    } else {
+        echo "<p>Could not find user info.</p>";
+    }
+    // Close database connection.
+    $db->close();
+}
+
+function getNextEvent() {
+    // Connect to database.
+    $db = new mysqli("student.cs.hioa.no", "s236305", "", "s236305");
+    if ($db->connect_error) {
+        trigger_error($db->connect_error);
+    }
+    // Execute SQL query.
+    $sql = "SELECT description, datetime, gender, sport "
+        . "FROM Event "
+        . "ORDER BY datetime ASC "
+        . "LIMIT 1";
+    $result = $db->query($sql);
+    // Echo out the next event.
+    if($db->affected_rows>0) {
+        $row = $result->fetch_object();
+        $date = DateTime::createFromFormat("m-d-y", $row->datetime);
+        $dateString = $date->format("l, F jS Y");
+        if ($row->gender == "Male") {
+            $gender = "Men's";
+        } else {
+            $gender = "Women's";
+        }
+        echo "<p>$dateString</p>"
+            . "<h4>$row->description</h4>"
+            . "<p>$gender $row->sport</p>";
+    } else {
+        echo "<p>There are no more events</p>";
+    }
+    // Close database connection.
+    $db->close();
+}
+
+//======================================================================
+// POPULATE FUNCTIONS
+//======================================================================
 
 function populateEventsDropdown() {
     // Connect to database.
@@ -20,22 +113,59 @@ function populateEventsDropdown() {
     $sql = "SELECT EventID, gender, sport, description, datetime "
              . "FROM Event "
              . "ORDER BY datetime ASC;";
-    $resultat = $db->query($sql);
+    $result = $db->query($sql);
     // Echo out all rows.
     if($db->affected_rows>0) {
-        $antallRader = $db->affected_rows;
-        for ($i=0;$i<$antallRader;$i++) {
-            $rad = $resultat->fetch_object();
-            $date = DateTime::createFromFormat("m-d-y", $rad->datetime);
+        $numRows = $db->affected_rows;
+        for ($i=0;$i<$numRows;$i++) {
+            $row = $result->fetch_object();
+            $date = DateTime::createFromFormat("m-d-y", $row->datetime);
             $dateString = $date->format("l, F jS Y");
-            if ($rad->gender == "Male") {
+            if ($row->gender == "Male") {
                 $gender = "Men's";
             } else {
                 $gender = "Women's";
             }
-            echo "<option value='$rad->EventID'>$gender $rad->sport - $rad->description ($dateString)</option>";
+            echo "<option value='$row->EventID'>$gender $row->sport - $row->description ($dateString)</option>";
         }
     }
+    // Close database connection.
+    $db->close();
+}
+
+function populateEventSpectatorTable() {
+    // Connect to database.
+    $db = new mysqli("student.cs.hioa.no", "s236305", "", "s236305");
+    if ($db->connect_error) {
+        trigger_error($db->connect_error);
+    }
+    // Get the ID of the logged in spectator. TODO
+    $id = 1;
+    // Execute SQL query.
+    $sql = "SELECT description, datetime, gender, sport "
+        . "FROM Event "
+        . "JOIN EventSpectator ON Event.EventID = EventSpectator.Event "
+        . "WHERE EventSpectator.Spectator LIKE $id "
+        . "ORDER BY datetime ASC;";
+    $result = $db->query($sql);
+    // Echo out all rows.
+    if ($db->affected_rows > 0) {
+        $numRows = $db->affected_rows;
+        for ($i = 0; $i < $numRows; $i++) {
+            $row = $result->fetch_object();
+            $date = DateTime::createFromFormat("m-d-y", $row->datetime);
+            $dateString = $date->format("F jS Y");
+            if ($row->gender == "Male") {
+                $gender = "Men's";
+            } else {
+                $gender = "Women's";
+            }
+            echo "<tr><td>$dateString</td>"
+                . "<td>$gender $row->sport</td>"
+                . "<td>$row->description</td></tr>";
+        }
+    }
+    // Close database connection.
     $db->close();
 }
 
@@ -50,152 +180,38 @@ function populateEventsTable() {
          . "FROM Event "
          . "ORDER BY datetime ASC;";
 
-    $resultat = $db->query($sql);
+    $result = $db->query($sql);
     // Echo out all rows.
     if ($db->affected_rows > 0) {
-        $antallRader = $db->affected_rows;
-        for ($i = 0; $i < $antallRader; $i++) {
-            $rad = $resultat->fetch_object();
-            $date = DateTime::createFromFormat("m-d-y", $rad->datetime);
+        $numRows = $db->affected_rows;
+        for ($i = 0; $i < $numRows; $i++) {
+            $row = $result->fetch_object();
+            $date = DateTime::createFromFormat("m-d-y", $row->datetime);
             $dateString = $date->format("F jS Y");
-            if ($rad->gender == "Male") {
+            if ($row->gender == "Male") {
                 $gender = "Men's";
             } else {
                 $gender = "Women's";
             }
             echo "<tr><td>$dateString</td>"
-                . "<td>$gender $rad->sport</td>"
-                . "<td>$rad->description</td></tr>";
+                . "<td>$gender $row->sport</td>"
+                . "<td>$row->description</td></tr>";
         }
     }
     // Close database connection.
     $db->close();
 }
 
-function populateEventSpectatorTable() {
-    // Connect to database.
-    $db = new mysqli("student.cs.hioa.no", "s236305", "", "s236305");
-    if ($db->connect_error) {
-        trigger_error($db->connect_error);
-    }
-    // TODO: get logged in spectator id
-    $id = 1;
-    // Execute SQL query.
-    $sql = "SELECT description, datetime, gender, sport "
-         . "FROM Event "
-         . "JOIN EventSpectator ON Event.EventID = EventSpectator.Event "
-         . "WHERE EventSpectator.Spectator LIKE $id "
-         . "ORDER BY datetime ASC;";
-    $resultat = $db->query($sql);
-    // Echo out all rows.
-    if ($db->affected_rows > 0) {
-        $antallRader = $db->affected_rows;
-        for ($i = 0; $i < $antallRader; $i++) {
-            $rad = $resultat->fetch_object();
-            $date = DateTime::createFromFormat("m-d-y", $rad->datetime);
-            $dateString = $date->format("F jS Y");
-            if ($rad->gender == "Male") {
-                $gender = "Men's";
-            } else {
-                $gender = "Women's";
-            }
-            echo "<tr><td>$dateString</td>"
-                . "<td>$gender $rad->sport</td>"
-                . "<td>$rad->description</td></tr>";
-        }
-    }
-    // Close database connection.
-    $db->close();
-}
+//======================================================================
+// REGISTER FUNCTIONS
+//======================================================================
 
-// GET
+//======================================================================
+// OTHER FUNCTIONS TODO: Fix and categorize these
+//======================================================================
 
-function getAdminInfo() {
-    $db = new mysqli("student.cs.hioa.no", "s236305", "", "s236305");
-    if ($db->connect_error) {
-        trigger_error($db->connect_error);
-    }
 
-    // TODO: Get logged in admin id
-    $id = 1;
-
-    $sql = "SELECT username, firstname, lastname, phonenumber "
-        . "FROM Admin "
-        . "WHERE AdminID = $id;";
-    $resultat = $db->query($sql);
-
-    if($db->affected_rows>0) {
-        $rad = $resultat->fetch_object();
-        echo "<h4>Username:</h4><p>$rad->username</p>"
-           . "<h4>Name:</h4><p>$rad->firstname $rad->lastname</p>"
-           . "<h4>Phone number:</h4><p>$rad->phonenumber</p>";
-    } else {
-        echo "<p>Could not find admin info.</p>";
-    }
-
-    $db->close();
-}
-
-function getSpectatorInfo() {
-    $db = new mysqli("student.cs.hioa.no", "s236305", "", "s236305");
-    if ($db->connect_error) {
-        trigger_error($db->connect_error);
-    }
-
-    // TODO: Get logged in spectator id
-    $id = 1;
-
-    $sql = "SELECT firstname, lastname, phonenumber, email, username "
-        . "FROM Spectator "
-        . "WHERE SpectatorID = $id;";
-    $resultat = $db->query($sql);
-
-    if($db->affected_rows>0) {
-        $rad = $resultat->fetch_object();
-        echo "<h4>Username:</h4><p>$rad->username</p>"
-            . "<h4>Name:</h4><p>$rad->firstname $rad->lastname</p>"
-            . "<h4>Phone number:</h4><p>$rad->phonenumber</p>"
-            . "<h4>E-mail:</h4><p>$rad->email</p>";
-    } else {
-        echo "<p>Could not find user info.</p>";
-    }
-
-    $db->close();
-}
-
-function getNextEvent() {
-    $db = new mysqli("student.cs.hioa.no", "s236305", "", "s236305");
-    if ($db->connect_error) {
-        trigger_error($db->connect_error);
-    }
-
-    $sql = "SELECT description, datetime, gender, sport "
-        . "FROM Event "
-        . "ORDER BY datetime ASC "
-        . "LIMIT 1";
-    $resultat = $db->query($sql);
-
-    if($db->affected_rows>0) {
-        $rad = $resultat->fetch_object();
-        $date = DateTime::createFromFormat("m-d-y", $rad->datetime);
-        $dateString = $date->format("l, F jS Y");
-        if ($rad->gender == "Male") {
-            $gender = "Men's";
-        } else {
-            $gender = "Women's";
-        }
-        echo "<p>$dateString</p>"
-            . "<h4>$rad->description</h4>"
-            . "<p>$gender $rad->sport</p>";
-    } else {
-        echo "<p>There are no more events</p>";
-    }
-    $db->close();
-}
-
-// OTHER
-
-$db = new mysqli("student.cs.hioa.no", "s236305", "", "s236305");
+/*$db = new mysqli("student.cs.hioa.no", "s236305", "", "s236305");
 if ($db->connect_error) {
     echo "Feil i databasetilknytningen";
     trigger_error($db->connect_error);
@@ -350,9 +366,11 @@ function registerEventSpectator() {
         ob_flush();
     }
     //mysqli_close($db); #TODO: LUKKE HER?
-}
+}*/
 
-/*function listAllSpectators() {
+/*  TODO: DO WE NEED THESE??
+ *
+ * function listAllSpectators() {
 
     $sql = "Select username, firstname, lastname, phonenumber, email from Spectator ";
     $resultat = $db->query($sql);
