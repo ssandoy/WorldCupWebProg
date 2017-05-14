cd45cbf7e60ce0fe864bd6cbb9de030db4190c0c
Sindre Beba <sindre.beba@gmail.com>
2017-05-14 20:53:18 +0200

    Designet login skjerm.

--- login.php
+++ login.php
@@ -1,50 +1,53 @@
 <!DOCTYPE html>
-
 <html>
-
 <head>
-    <title>Login Page</title>
-    <style type = "text/css">
-        body {
-            font-family:Arial, Helvetica, sans-serif;
-            font-size:14px;
-        }
-        label {
-            font-weight:bold;
-            width:100px;
-            font-size:14px;
-        }
-        .box {
-            border:#666666 solid 1px;
-        }
-    </style>
-    <?php include("assets/authorization.php"); ?>
+    <!-- jQuery -->
+    <script src="assets/javascript/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
+    <script src="assets/javascript/bootstrap.min.js"></script>
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/stylesheet.css"/>
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed" rel="stylesheet">
+    <!-- PHP -->
+    <?php
+    include("assets/authorization.php");
+    ?>
+    <!-- Metadata -->
+    <title>WSC - Seefeld 2019</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Sindre Beba, Sander Sandøy">
 </head>
-
-<body bgcolor = "#FFFFFF">
-<!-- Authorization -->
-<?php validateNotLoggedIn() ?>
-    <div align = "center">
-        <div style = "width:300px; border: solid 1px #333333; " align = "left">
-            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
-            <div style = "margin:30px">
-                <form method="POST" action="">
-                    <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
-                    <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
-                    <label>Admin :</label><input type="checkbox" name="adminLogin"/><br/><br />
-                    <input name="loginSubmit" type="submit" value="Submit"/><br />
-                </form>
-                <div style = "font-size:11px; color:#cc0000; margin-top:10px">
-                    <?php
-                        if (isset($_POST["loginSubmit"])) {
-                            if (isset($_POST["adminLogin"])) {
-                                checkAdminLogin();
-                            } else {
-                                checkSpectatorLogin();
-                            }
+<body>
+    <!-- Authorization -->
+    <?php validateNotLoggedIn() ?>
+    <div class="loginWindow">
+        <div class="loginHeader">
+            <b>Login</b>
+        </div>
+        <div class="loginBody">
+            <form method="POST" action="">
+                <p>Username</p>
+                <input class="form-control" name="username" type="text">
+                <p>Password</p>
+                <input class="form-control" name="password" type="password">
+                <div class='checkbox'><label><input name="adminLogin" type='checkbox'>Admin</label></div>
+                <input class="btn btn-default" name="loginSubmit" type="submit" value="Log in"/><br />
+            </form>
+            <br/>
+            <p><a href="register.php">Don't have an account? Register here.</a></p>
+            <div class="loginMessage">
+                <?php
+                    if (isset($_POST["loginSubmit"])) {
+                        if (isset($_POST["adminLogin"])) {
+                            checkAdminLogin();
+                        } else {
+                            checkSpectatorLogin();
                         }
-                    ?>
-                </div>
+                    }
+                ?>
             </div>
         </div>
     </div>
